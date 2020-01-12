409e8124e968a1ec7a8fc8c006345327d4752f9f
myles1509 <mylesbecker1509@gmail.com>
2020-01-12 01:59:20 -0700

    fixed install.sh typos 01.12.2020

--- install.sh
+++ install.sh
@@ -38,50 +38,51 @@ echo
 read -p "Pass for sshkey: " sshPassVar
 export sshPassVar
 echo
-if [[$reply =~ ^[Yy]$ ]]
-	(
-		echo d
-		echo 6
-		echo d
-		echo 5
-		echo n
-		echo 5
-		echo 
-		echo +16G
-		echo t 
-		echo 5
-		echo 19
-		echo n
-		echo 6
-		echo 
-		echo 
-		echo w
-	) | fdisk /dev/sda
-	partprobe
-	echo "Current disk layout: "
-	fdisk -l /dev/sda
-	mkswap -L "archLinuxSwap" /dev/sda5
-	swapon /dev/sda5
-	echo "Swap check: "
-	free -m
-	(
-		echo y
-	) | mkfs.ext4 -L "archLinuxRoot" /dev/sda6
-	mount /dev/sda6 /mnt
-	pacstrap /mnt base base-devel linux linux-firmware vim
-	mkdir -p /mnt/boot/efi
-	mount /dev/sda2 /mnt/boot/efi
-	genfstab -p /mnt >> /mnt/etc/fstab
-	echo "Current fstab config: "
-	cat /mnt/etc/fstab
-	echo "Beginning chroot: "
-	echo
-	mkdir /MAIS
-	cp * /MAIS
-	chmod +x /MAIS/chroot.sh
-	arch-chroot /mnt  /MAIS/chroot.sh
-	echo "Chroot finished."
-	echo
-	read -p "Welcome to Arch $userNameVar." novar
+if [[ $reply =~ ^[Yy]$ ]]
+	then
+		(
+			echo d
+			echo 6
+			echo d
+			echo 5
+			echo n
+			echo 5
+			echo 
+			echo +16G
+			echo t 
+			echo 5
+			echo 19
+			echo n
+			echo 6
+			echo 
+			echo 
+			echo w
+		) | fdisk /dev/sda
+		partprobe
+		echo "Current disk layout: "
+		fdisk -l /dev/sda
+		mkswap -L "archLinuxSwap" /dev/sda5
+		swapon /dev/sda5
+		echo "Swap check: "
+		free -m
+		(
+			echo y
+		) | mkfs.ext4 -L "archLinuxRoot" /dev/sda6
+		mount /dev/sda6 /mnt
+		pacstrap /mnt base base-devel linux linux-firmware vim
+		mkdir -p /mnt/boot/efi
+		mount /dev/sda2 /mnt/boot/efi
+		genfstab -p /mnt >> /mnt/etc/fstab
+		echo "Current fstab config: "
+		cat /mnt/etc/fstab
+		echo "Beginning chroot: "
+		echo
+		mkdir /MAIS
+		cp * /MAIS
+		chmod +x /MAIS/chroot.sh
+		arch-chroot /mnt  /MAIS/chroot.sh
+		echo "Chroot finished."
+		echo
+		read -p "Welcome to Arch $userNameVar." novar
 	reboot
 fi
